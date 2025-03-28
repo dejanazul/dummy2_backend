2f7d06db8f4ac453de18f298fff3d7a326ca829e
dejanazul <dejanazoel@gmail.com>
2025-03-28 07:10:58 +0700

    supabase config 5

--- src/server.js
+++ src/server.js
@@ -1,11 +1,9 @@
 // const dotenv = require("dotenv");
-const http = require("http")
 const app = require("./app.js");
 
 // dotenv.config();
 const PORT = process.env.PORT || 3000;
-const server = http.createServer(app)
 
-server.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+app.listen(PORT, () => {
+  console.log(`Server running on ${PORT}`);
 });
